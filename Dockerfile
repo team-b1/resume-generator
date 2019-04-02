c055364ef98721a3fb76c383a3ac3a6a37927916
Brendan Horng <brendan.horng@ludusamo.com>
2019-04-01 23:34:53 -0400

    Add preliminary pdf generation

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,13 @@ WORKDIR /app
 
 COPY . /app
 
-RUN apt-get -y update
-RUN apt-get -y install wkhtmltopdf
-
 RUN pip install --trusted-host pypi.python.org -r requirements.txt
+RUN apt-get -y update
+RUN apt-get -y install wget wkhtmltopdf xz-utils libfontconfig1 libxrender1
+RUN cd ~
+RUN wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.3/wkhtmltox-0.12.3_linux-generic-amd64.tar.xz
+RUN tar vxf 'wkhtmltox-0.12.3_linux-generic-amd64.tar.xz'
+RUN cp wkhtmltox/bin/wk* /usr/local/bin/
 
 RUN useradd -m user
 USER user
--- app.py
+++ app.py
@@ -1,25 +1,32 @@
-from flask import Flask
+from flask import Flask, Response, request
 import os
 import socket
 import json
 import xml.etree.ElementTree as ET
+import markdown2
+import pdfkit
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
+@app.route("/", methods=['POST'])
+def pdf():
+    userData = request.get_json()
     htmlTree = None
-    with open('template/data.json', 'rb') as dataFile:
-        userData = json.load(dataFile)
-        print(userData)
-        htmlTree = ET.ElementTree()
-        htmlTree.parse('template/template.html')
-        for k,v in userData.items():
-            e = list(htmlTree.iter(k))[0]
-            e.text = v
-    ET.dump(htmlTree)
+    htmlTree = ET.ElementTree()
+    htmlTree.parse('static/template.html')
+    for k,v in userData.items():
+        e = list(htmlTree.iter(k))[0]
+        content = '<SectionContent>' + markdown2.markdown(v) + '</SectionContent>'
+        node = ET.fromstring(content)
+        e.append(node)
     html = ET.tostring(htmlTree.getroot(), method='html')
-    return html
+
+    css = 'static/template.css'
+    pdf = pdfkit.from_string(html.decode('utf-8'), False, css=css)
+
+    res = Response(pdf)
+    res.mimetype = 'application/pdf'
+    return res
 
 def main():
     app.run(host='0.0.0.0', port=os.getenv('PORT', 8000))
